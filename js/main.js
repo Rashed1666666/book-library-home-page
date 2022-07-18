fdd6c8c8e44d31239604cd084eec41384b4b6e9f
Mohamed Rashed <mohamedtolleba2016@gmail.com>
2022-07-18 04:08:27 +0200

    add project file

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// function hiddenImage(){
+//     document.getElementById("item-info").style.display ="block";
+// }
+// function displayImage() {
+//     document.getElementById("item-info").style.display = "none";
+//   }
+
+
+// function hideImage(x) {
+//     x.style.display = "block";
+//   }
+//   function showImage(x) {
+//     x.style.display = "none";
+//   }  
